bbf859afa01cbda23d48e556ec0e0b3299777ae6
Wyatt Kirby <kirby.wa@gmail.com>
2013-10-15 15:51:46 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,57 @@
-isTransparent
-=============
+# isTransparent
 
-A jQuery plugin to check image transparency.
+Backstretch is a simple jQuery plugin that allows you to add a dynamically-resized, slideshow-capable background image to any page or element. The image will stretch to fit the page/element, and will automatically resize as the window/element size changes.
+## Demo
+
+There are a couple of examples included with this package, or feel free to check it out live [on the project page itself](http://srobbin.com/jquery-plugins/backstretch/).
+
+## Setup
+
+Include the jQuery library (version 1.7 or newer) and Backstretch plugin files in your webpage (preferably at the bottom of the page, before the closing BODY tag):
+
+```html
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src="jquery.isTransparent.min.js"></script>
+
+<script>
+
+  // Basic Usage
+  // This will return true if the image is transparent, and
+  // false if parts of the image are opaque.
+
+  $("img").isTransparent();
+
+  // You can also set options which will determine the speed
+  // and threshold for transparency.
+  
+  $("img").isTransparent({
+    opacityThreshold: 125,
+    quality: 5
+  });
+
+  // If you'd like to set the defaults for these settings, rather
+  // than setting them each time you call the function, you can
+  // do so by adjusting the variables before calling isTransparent()
+
+  $.fn.isTransparent.defaults.opacityThreshold: 200;
+  $.fn.isTransparent.defaults.quality: 0;
+
+</script>
+```
+
+## Options
+
+| Name | Description | Type | Default |
+|------|-------------|------|---------|
+| `opacityThreshold` | The minimum threshold to be considered opaque. Operates on a scale of 0 - 255, where 255 is fully opaque. | Int | 125 |
+| `quality` | The quality at which you'd like to check the image. Higher numbers run faster, but will be less accurate in determining transparency. | Int | 5 |
+
+## Changelog
+
+### Version 1.0
+
+* Initial release
+
+## Thanks To
+
+Many thanks to Color Thief by Lokesh Dhakar (http://lokeshdhakar.com/projects/color-thief/) for the code which traverses the images pixels checking for alpha values.
---
+++ jquery.isTransparent.js
@@ -0,0 +1,116 @@
+/*!
+ * jQuery isTransparent - v1.0 - 2013-10-15
+ *
+ * Returns true or false based on image transparency.
+ *
+ * Author: Wyatt Kirby <wyatt@genius.com>
+ * Licensed under the MIT license
+ */
+
+;(function($, window, document, undefined) {
+    'use strict';
+
+    // Set up jQuery Plugin
+    // ----------------------------------------------------
+
+    $.fn.isTransparent = function(options) {
+        var retVal = false;
+
+        this.each(function() {
+            if (!$.data(this, 'plugin_isTransparent')) {
+                $.data(this, 'plugin_isTransparent', true);
+                retVal = new TransparencyCheck(this, options).isTransparent;
+            }
+        });
+
+        return retVal;
+    };
+
+    function TransparencyCheck(element, options) {
+        this.image = element;
+        this.isTransparent = false;
+        this.options = $.extend(true, {}, $.fn.isTransparent.defaults, options);
+
+        this.init();
+    }
+
+    // Plugin Constructor
+    // ----------------------------------------------------
+
+    TransparencyCheck.prototype.init = function() {
+
+        if (!$(this.image).is("img")) {
+            throw new Error("Element is not an image");
+        }
+
+        var quality = this.options.quality,
+            image = new CanvasImage(this.image),
+            imageData = image.getImageData(),
+            pixels = imageData.data,
+            pixelCount = image.getPixelCount(),
+            pixelArray = [];
+
+        // Check color of each pixel, store it in an array if it meets minimum
+        // threshold for "visibility."
+
+        for (var i = 0, offset, r, g, b, a; i < pixelCount; i = i + quality) {
+            offset = i * 4;
+            r = pixels[offset + 0];
+            g = pixels[offset + 1];
+            b = pixels[offset + 2];
+            a = pixels[offset + 3];
+
+            // If pixel is mostly opaque and not white
+            // add it to the array.
+            if (a >= this.options.opacityThreshold) {
+                if (!(r > 250 && g > 250 && b > 250)) {
+                    pixelArray.push([r, g, b]);
+                }
+            }
+        }
+
+        // Clean up
+        image.removeCanvas();
+
+        // If the array is empty, nothing registered as a visible color
+        // so we can set our transparency var to true.
+
+        this.isTransparent = pixelArray.length ? false : true;
+    };
+
+    // User Accessible Defaults
+    // ----------------------------------------------------
+
+    $.fn.isTransparent.defaults = {
+        opacityThreshold: 125,
+        quality: 5
+    };
+
+    // Canvas Utility Class
+    // ----------------------------------------------------
+
+    var CanvasImage = function(image) {
+        this.canvas = document.createElement('canvas');
+        this.context = this.canvas.getContext('2d');
+
+        document.body.appendChild(this.canvas);
+
+        this.width = this.canvas.width = image.width;
+        this.height = this.canvas.height = image.height;
+
+        this.context.drawImage(image, 0, 0, this.width, this.height);
+    };
+
+    CanvasImage.prototype.getPixelCount = function() {
+        return this.width * this.height;
+    };
+
+    CanvasImage.prototype.getImageData = function() {
+        return this.context.getImageData(0, 0, this.width, this.height);
+    };
+
+    CanvasImage.prototype.removeCanvas = function() {
+        this.canvas.parentNode.removeChild(this.canvas);
+    };
+
+})(jQuery, window, document);
